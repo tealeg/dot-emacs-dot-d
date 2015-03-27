fc697ef56a9a2027340bced450aa2a262bbd1da7
Geoffrey J. Teale <geoffrey.teale@canonical.com>
2015-03-27 08:58:30 +0100

    Remove graphical guard as this issue only affects emacs when X11 support is not compiled in.

--- lisp/tealeg-frame.el
+++ lisp/tealeg-frame.el
@@ -4,11 +4,10 @@
 ;;;     Frame setup
 
 ;;; Code:
-(when (display-graphic-p)
-	(menu-bar-mode -1)
-	(scroll-bar-mode -1)
-	(horizontal-scroll-bar-mode -1)
-	(tool-bar-mode -1))
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+(horizontal-scroll-bar-mode -1)
+(tool-bar-mode -1)
 (line-number-mode 1)
 (column-number-mode 1)
 (put 'suspend-frame 'disabled t)
