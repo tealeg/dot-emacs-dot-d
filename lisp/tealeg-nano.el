fce6e7dbc07f6ab4d4750e9a5ab09200029209c3
Geoffrey J. Teale <tealeg@gmail.com>
2020-12-09 13:27:51 +0100

    Use bigger fonts

--- lisp/tealeg-nano.el
+++ lisp/tealeg-nano.el
@@ -6,12 +6,15 @@
 (package-initialize)
 (add-to-list 'load-path ".")
 
+(set-face-font 'default "Share Tech Mono-18")
 (require 'nano-layout)
 (require 'nano-theme-dark)
 ;;(require 'nano-theme-light)
 (require 'nano-modeline)
 (require 'nano-help)
 (require 'nano-splash)
+(setf (alist-get 'font default-frame-alist) "Share Tech Mono:size=18")
+
 
 (provide 'tealeg-nano)
 ;;; tealeg-nano.el ends here
