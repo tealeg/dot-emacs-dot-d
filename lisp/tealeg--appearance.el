1ddf6f7ac30f1c09d78f9432d54cc647d828fe87
Geoffrey J. Teale <tealeg@gmail.com>
2021-12-09 08:05:12 +0100

    Support dark mode

--- lisp/tealeg--appearance.el
+++ lisp/tealeg--appearance.el
@@ -13,21 +13,61 @@
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
 (column-number-mode 1)
+
+(setq tealeg--theme-tone 'dark)
+
 ;; (doom-modeline-mode -1)
 (custom-set-default 'mode-line-compact t)
 (cond
  ((string-equal system-type "darwin")
     (progn
       (message "Loading Mac OS X apperance parameters")
-      (load-theme 'monochrome-bright t)
-      (add-to-list 'default-frame-alist '(font . "IBM Plex Mono-14"))
-      (set-frame-font "IBM Plex Mono-14")
-      (set-face-font 'variable-pitch "IBM Plex Sans-14")
-      (set-face-font 'mode-line "IBM Plex Serif-12")
-      (set-face-font 'mode-line-inactive "IBM Plex Serif-12")
-      (set-face-attribute 'fringe nil :background "#ffffff")
-      (set-face-attribute 'mode-line nil :box (list :line-width 5 :color "white" :style nil ) :overline "black" :underline nil :background "white")
-      (set-face-attribute 'mode-line-inactive nil :box (list :line-width 5 :color "white" :style nil ) :overline "black" :underline nil :background "white" :foreground "gray50")
+      (if (eq tealeg--theme-tone 'dark)
+	  (progn
+	    (load-theme 'monochrome t)
+	    (set-face-attribute 'fringe nil
+				:foreground "#ffffff"
+				:background "#000000")
+	    (set-face-attribute 'mode-line nil
+				:box (list :line-width 5
+					   :color "black"
+					   :style nil )
+				:overline "#cccccc"
+				:background "black"
+				:foreground "#cccccc")
+	    (set-face-attribute 'mode-line-inactive nil
+				:box (list :line-width 5
+					   :color "black"
+					   :style nil )
+				:overline "white"
+				:background "black"
+				:foreground "gray50")
+	    )
+	(progn 
+	    (load-theme 'monochrome-bright t)
+	    (set-face-attribute 'fringe nil
+				:background "#ffffff"
+				:foreground "#444444")
+	    (set-face-attribute 'mode-line nil
+				:box (list :line-width 5
+					   :color "white"
+					   :style nil )
+				:overline "black"
+				:underline nil
+				:background "white")
+	    (set-face-attribute 'mode-line-inactive nil
+				:box (list :line-width 5
+					   :color "white"
+					   :style nil )
+				:overline "black"
+				:background "white"
+				:foreground "gray50")
+))
+      (add-to-list 'default-frame-alist '(font . "Ubuntu Mono-15"))
+      (set-frame-font "Ubuntu Mono-15")
+      (set-face-font 'variable-pitch "Ubuntu-15")
+      (set-face-font 'mode-line "Ubuntu-12")
+      (set-face-font 'mode-line-inactive "Ubuntu-12")
       (set-fringe-style '( 40 . 40 ))
 
       ))
@@ -40,7 +80,7 @@
     (set-face-font 'mode-line "IBM Plex Serif-10")
     (set-face-font 'mode-line-inactive "IBM Plex Serif-10")
     (menu-bar-mode -1)
-      (set-face-attribute 'fringe nil :background "#ffffff")
+      (set-face-attribute 'fringe nil :background "#ffffff" :foreground "#444444")
       (set-face-attribute 'mode-line nil :box (list :line-width 3 :color "white" :style nil ) :overline "black" :underline nil :background "white")
       (set-face-attribute 'mode-line-inactive nil :box (list :line-width 3 :color "white" :style nil ) :overline "black" :underline nil :background "white" :foreground "gray50")
       (set-fringe-style '( 20 . 20 ))
