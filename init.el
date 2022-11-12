5c42bef7f7952f1192569de8eb5a77e17525ba98
Geoffrey J. Teale <tealeg@gmail.com>
2022-11-12 06:20:18 +0100

    Remove carp

--- init.el
+++ init.el
@@ -29,11 +29,9 @@
 (require 'tealeg--eshell)
 (require 'tealeg--guix)
 (require 'tealeg--pollen)
-(require 'tealeg--carp)
 ;; tabs have to come before appearance
 ;;(require 'tealeg--tabs)
 (require 'tealeg--appearance)
-
 (require 'tealeg--scheme)
 (require 'tealeg--org)
 
