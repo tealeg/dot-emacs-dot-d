4eef19b66cd88630d16c5e406e250a83223e7bfe
Geoffrey J. Teale <geoffrey@upvest.co>
2023-07-25 11:24:21 +0200

    Remove python venv stuff from eshell

--- lisp/tealeg--markdown.el
+++ lisp/tealeg--markdown.el
@@ -1,5 +1,17 @@
-(use-package markdown-mode)
 (use-package flymake-markdownlint)
 (use-package flymd)
+;; (use-package langtool)
+(require 'flyspell)
+
+(use-package markdown-mode
+  :config 
+
+  (progn
+    (defun tealeg--markdown-mode-helper-f ()
+      (flyspell-mode 1)
+      ;; (add-hook 'after-save-hook 'langtool-check nil 'make-it-local)
+      )
+
+    (add-to-list 'markdown-mode-hook #'tealeg--markdown-mode-helper-f)))
 
 (provide 'tealeg--markdown)
