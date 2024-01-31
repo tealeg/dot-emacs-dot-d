29d7b1731c8588c91162efa8dbddab8e48aa85ab
Geoffrey J. Teale <geoffrey@upvest.co>
2024-01-31 11:39:40 +0100

    Seems to be more important to have web-mode support than markdown

--- lisp/tealeg--markdown.el
+++ lisp/tealeg--markdown.el
@@ -1,14 +1,21 @@
 
 (use-package flymake-markdownlint)
-(use-package markdown-mode)
+;; (use-package markdown-mode)
+(use-package web-mode)
 
 (elpaca nil
   (require 'flyspell)
+  (require 'web-mode)
+  (require 'electric)
 
   (defun tealeg--markdown-mode-helper-f ()
     (flyspell-mode 1)
     (flymake-markdownlint-setup)
-    (flymake-mode 1))
+    (flymake-mode 1)
+    (web-mode 1)
+    (web-mode-set-engine 'django)
+    (electric-pair-mode 1)
+    )
 
   (add-hook 'markdown-mode-hook #'tealeg--markdown-mode-helper-f))
 
