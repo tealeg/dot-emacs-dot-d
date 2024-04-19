d51cd4bd8cf9f9a7589e88c5a50005c3d28477e0
Geoffrey J. Teale <geoffrey@upvest.co>
2024-04-19 15:57:03 +0200

    Setup racket-mode properly

--- lisp/tealeg--racket.el
+++ lisp/tealeg--racket.el
@@ -1,8 +1,8 @@
-(use-package racket-mode
-  :config
+(use-package racket-mode)
 
+(elpaca nil
   (defun tealeg--racket-mode-helper-f ()
-    (require racket-xp-mode)
+    (require 'racket-xp)
     (racket-xp-mode 1))
 
   (add-hook 'racket-mode-hook #'tealeg--racket-mode-helper-f)
