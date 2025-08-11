f84caf12037e0ed7e58174e8791606ca0b7b4e0a
Geoffrey J. Teale <geoffrey@upvest.co>
2025-08-11 11:10:28 +0200

    Don't do security prompts for org-babel

--- init.el
+++ init.el
@@ -206,6 +206,9 @@
 (use-package ob-mermaid
   :ensure t
   :config
+  
+  (setq org-confirm-babel-evaluate nil)
+  
   (org-babel-do-load-languages
     'org-babel-load-languages
     '((mermaid . t)
